cc2bb850e2257dcbe062d31763a5ce9a7054e6c2
boruch d <dubywider@gmail.com>
2019-05-27 17:06:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -23,26 +23,28 @@ class Song
    def self.artist
      @@artist.uniq
    end
-   def self.genre_count
-     genre_count = {}
-    @@genres.each do |gen|
-    if genre_count[gen]
-      genre_count[gen] += 1 
-    else 
-      genre_count = 1 
-      end 
-    end 
+  def self.genre_count
+    genre_count = {}
+    @@genres.each do |genre| 
+      if genre_count[genre]
+      genre_count[genre] += 1 
+      else
+      genre_count[genre] = 1
+      end
+    end
     genre_count
-end
-def self.artist_count
-  artist_count = {}
-  @@artist.each do |art|
-    if artist_count[art]
-      artist_count[art] +=1
-    else 
-      artist_count[art] = 1
-      end 
+  end
+
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
+      artist_count[artist] +=1
+      else
+      artist_count[artist] = 1
+      end
     end
     artist_count
-  end 
-end 
+  end
+
+end
