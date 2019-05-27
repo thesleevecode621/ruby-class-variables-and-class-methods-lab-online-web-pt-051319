c151ccba198ce4a423919c972207f3e037895c94
boruch d <dubywider@gmail.com>
2019-05-27 17:26:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,13 @@
 class Song 
   @@count = 0
-  
+   def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @@artists << artist
+    @genre = genre
+    @@genres << genre
+    @@count += 1
+  end 
   def self.count 
     @@count
   end 
@@ -43,6 +50,6 @@ class Song
   end
   
  
-  end
+
   attr_accessor :name, :artist, :genre
 end 
