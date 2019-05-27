2b61274513e9832194db004aa5c0eaf3db3222be
boruch d <dubywider@gmail.com>
2019-05-27 16:42:53 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,17 @@
+class Song
 
+  attr_accessor :name, :artist, :genre
+
+  @@song_count = 0
+  @@genres = []
+  @@artists = []
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@genres << @genre
+    @@artists << @artist
+    @@song_count +=1
+  end
+end
